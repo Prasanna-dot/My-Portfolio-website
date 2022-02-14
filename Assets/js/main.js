654da3879adf333afdfde77ed2c2881735e02fd0
Prasanna-dot <prasannavenkatesh20.01.2k1@gmail.com>
2022-02-14 08:34:44 +0530

    Add

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,154 +1,156 @@
-// const loader = document.getElementById("preLoader");
-// window.addEventListener("load", function () {
-//   loader.classList.add("preloader");
-//   setTimeout(function () {
-//     loader.style.display = "none";
-//   }, 2500);
-// });
+// Preloader function
 
-let myPanel = document.getElementById("profile");
-let subpanel = document.getElementById("profileImg");
+const loader = document.getElementById("preLoader");
+window.addEventListener("load", function () {
+  loader.classList.add("preloader");
+  setTimeout(function () {
+    loader.style.display = "none";
+  }, 2500);
+});
 
-myPanel.onmousemove = transformPanel;
-myPanel.onmouseenter = handleMouseEnter;
-myPanel.onmouseleave = handleMouseLeave;
+// let myPanel = document.getElementById("profile");
+// let subpanel = document.getElementById("profileImg");
 
-let mouseX, mouseY;
+// myPanel.onmousemove = transformPanel;
+// myPanel.onmouseenter = handleMouseEnter;
+// myPanel.onmouseleave = handleMouseLeave;
 
-let transformAmount = 5;
+// let mouseX, mouseY;
 
-function transformPanel(mouseEvent) {
-  mouseX = mouseEvent.pageX;
-  mouseY = mouseEvent.pageY;
+// let transformAmount = 5;
 
-  const centerX = myPanel.offsetLeft + myPanel.clientWidth / 2;
-  const centerY = myPanel.offsetTop + myPanel.clientHeight / 2;
+// function transformPanel(mouseEvent) {
+//   mouseX = mouseEvent.pageX;
+//   mouseY = mouseEvent.pageY;
 
-  const percentX = (mouseX - centerX) / (myPanel.clientWidth / 2);
-  const percentY = -((mouseY - centerY) / (myPanel.clientHeight / 2));
+//   const centerX = myPanel.offsetLeft + myPanel.clientWidth / 2;
+//   const centerY = myPanel.offsetTop + myPanel.clientHeight / 2;
 
-  subpanel.style.transform =
-    "perspective(500px) rotateY(" +
-    percentX * transformAmount +
-    "deg) rotateX(" +
-    percentY * transformAmount +
-    "deg)";
-}
+//   const percentX = (mouseX - centerX) / (myPanel.clientWidth / 2);
+//   const percentY = -((mouseY - centerY) / (myPanel.clientHeight / 2));
 
-function handleMouseEnter() {
-  setTimeout(() => {
-    subpanel.style.transition = "";
-  }, 100);
-  subpanel.style.transition = "transform 0.1s";
-}
+//   subpanel.style.transform =
+//     "perspective(500px) rotateY(" +
+//     percentX * transformAmount +
+//     "deg) rotateX(" +
+//     percentY * transformAmount +
+//     "deg)";
+// }
 
-function handleMouseLeave() {
-  subpanel.style.transition = "transform 0.1s";
-  setTimeout(() => {
-    subpanel.style.transition = "";
-  }, 100);
+// function handleMouseEnter() {
+//   setTimeout(() => {
+//     subpanel.style.transition = "";
+//   }, 100);
+//   subpanel.style.transition = "transform 0.1s";
+// }
 
-  subpanel.style.transform = "perspective(400px) rotateY(0deg) rotateX(0deg)";
-}
+// function handleMouseLeave() {
+//   subpanel.style.transition = "transform 0.1s";
+//   setTimeout(() => {
+//     subpanel.style.transition = "";
+//   }, 100);
 
-function startingChatting() {
-  document.getElementById("intro").classList.add("introAfter");
-  document.getElementById("intro").classList.remove("introBefore");
-  document.getElementById("profile").classList.add("profileAfter");
-  document.getElementById("profile").classList.remove("profileBefore");
-  document.getElementById("form").classList.add("afterform");
-  document.getElementById("form").classList.remove("beforeform");
-  document.getElementById("chatDiv").classList.add("chatDivAfter");
-  document.getElementById("chatDiv").classList.remove("chatDivBefore");
-  document.getElementById("startButton").style.display = "none";
-}
+//   subpanel.style.transform = "perspective(400px) rotateY(0deg) rotateX(0deg)";
+// }
 
-function buttonChange() {
-  document.getElementById(
-    "actionButton"
-  ).innerHTML = `<svg style="fill: rgb(255, 255, 255); width:40%;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-  <path
-      d="M511.6 36.86l-64 415.1c-1.5 9.734-7.375 18.22-15.97 23.05c-4.844 2.719-10.27 4.097-15.68 4.097c-4.188 0-8.319-.8154-12.29-2.472l-122.6-51.1l-50.86 76.29C226.3 508.5 219.8 512 212.8 512C201.3 512 192 502.7 192 491.2v-96.18c0-7.115 2.372-14.03 6.742-19.64L416 96l-293.7 264.3L19.69 317.5C8.438 312.8 .8125 302.2 .0625 289.1s5.469-23.72 16.06-29.77l448-255.1c10.69-6.109 23.88-5.547 34 1.406S513.5 24.72 511.6 36.86z" />
-</svg>`;
+// function startingChatting() {
+//   document.getElementById("intro").classList.add("introAfter");
+//   document.getElementById("intro").classList.remove("introBefore");
+//   document.getElementById("profile").classList.add("profileAfter");
+//   document.getElementById("profile").classList.remove("profileBefore");
+//   document.getElementById("form").classList.add("afterform");
+//   document.getElementById("form").classList.remove("beforeform");
+//   document.getElementById("chatDiv").classList.add("chatDivAfter");
+//   document.getElementById("chatDiv").classList.remove("chatDivBefore");
+//   document.getElementById("startButton").style.display = "none";
+// }
 
-  let myForm = document.getElementById("chatform");
-  myForm.setAttribute("onsubmit", "chatsubmmit()");
+// function buttonChange() {
+//   document.getElementById(
+//     "actionButton"
+//   ).innerHTML = `<svg style="fill: rgb(255, 255, 255); width:40%;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+//   <path
+//       d="M511.6 36.86l-64 415.1c-1.5 9.734-7.375 18.22-15.97 23.05c-4.844 2.719-10.27 4.097-15.68 4.097c-4.188 0-8.319-.8154-12.29-2.472l-122.6-51.1l-50.86 76.29C226.3 508.5 219.8 512 212.8 512C201.3 512 192 502.7 192 491.2v-96.18c0-7.115 2.372-14.03 6.742-19.64L416 96l-293.7 264.3L19.69 317.5C8.438 312.8 .8125 302.2 .0625 289.1s5.469-23.72 16.06-29.77l448-255.1c10.69-6.109 23.88-5.547 34 1.406S513.5 24.72 511.6 36.86z" />
+// </svg>`;
 
-  let check = document.getElementById("textArea").value;
-  if (check == "") {
-    document.getElementById(
-      "actionButton"
-    ).innerHTML = `<svg style="fill: rgb(255, 255, 255); width:40%;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M192 352c53.03 0 96-42.97 96-96v-160c0-53.03-42.97-96-96-96s-96 42.97-96 96v160C96 309 138.1 352 192 352zM344 192C330.7 192 320 202.7 320 215.1V256c0 73.33-61.97 132.4-136.3 127.7c-66.08-4.169-119.7-66.59-119.7-132.8L64 215.1C64 202.7 53.25 192 40 192S16 202.7 16 215.1v32.15c0 89.66 63.97 169.6 152 181.7V464H128c-18.19 0-32.84 15.18-31.96 33.57C96.43 505.8 103.8 512 112 512h160c8.222 0 15.57-6.216 15.96-14.43C288.8 479.2 274.2 464 256 464h-40v-33.77C301.7 418.5 368 344.9 368 256V215.1C368 202.7 357.3 192 344 192z" /></svg>`;
+//   let myForm = document.getElementById("chatform");
+//   myForm.setAttribute("onsubmit", "chatsubmmit()");
 
-    myForm.setAttribute("onsubmit", "chatsubmmit()");
-  }
-}
+//   let check = document.getElementById("textArea").value;
+//   if (check == "") {
+//     document.getElementById(
+//       "actionButton"
+//     ).innerHTML = `<svg style="fill: rgb(255, 255, 255); width:40%;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M192 352c53.03 0 96-42.97 96-96v-160c0-53.03-42.97-96-96-96s-96 42.97-96 96v160C96 309 138.1 352 192 352zM344 192C330.7 192 320 202.7 320 215.1V256c0 73.33-61.97 132.4-136.3 127.7c-66.08-4.169-119.7-66.59-119.7-132.8L64 215.1C64 202.7 53.25 192 40 192S16 202.7 16 215.1v32.15c0 89.66 63.97 169.6 152 181.7V464H128c-18.19 0-32.84 15.18-31.96 33.57C96.43 505.8 103.8 512 112 512h160c8.222 0 15.57-6.216 15.96-14.43C288.8 479.2 274.2 464 256 464h-40v-33.77C301.7 418.5 368 344.9 368 256V215.1C368 202.7 357.3 192 344 192z" /></svg>`;
 
-let chatreply = [];
-localStorage.setItem("reply", JSON.stringify(chatreply));
+//     myForm.setAttribute("onsubmit", "chatsubmmit()");
+//   }
+// }
 
-function chatsubmmit() {
-  event.preventDefault();
-  let textArea = document.getElementById("textArea").value;
-  document.getElementById("textArea").value = "";
+// let chatreply = [];
+// localStorage.setItem("reply", JSON.stringify(chatreply));
 
-  let storeValue = JSON.parse(localStorage.getItem("reply"));
+// function chatsubmmit() {
+//   event.preventDefault();
+//   let textArea = document.getElementById("textArea").value;
+//   document.getElementById("textArea").value = "";
 
-  let len = storeValue.length;
+//   let storeValue = JSON.parse(localStorage.getItem("reply"));
 
-  if (len == 0 && textArea != "") {
-    chatreply.push(textArea);
-  } else if (storeValue[len - 1] != textArea && textArea != "") {
-    chatreply.push(textArea);
-  } else {
-    alert("Please chat With PV");
-  }
+//   let len = storeValue.length;
 
-  localStorage.setItem("reply", JSON.stringify(chatreply));
-  chatting();
-}
+//   if (len == 0 && textArea != "") {
+//     chatreply.push(textArea);
+//   } else if (storeValue[len - 1] != textArea && textArea != "") {
+//     chatreply.push(textArea);
+//   } else {
+//     alert("Please chat With PV");
+//   }
 
-function chatting() {
-  let storeValues = JSON.parse(localStorage.getItem("reply"));
+//   localStorage.setItem("reply", JSON.stringify(chatreply));
+//   chatting();
+// }
 
-  let right = "";
-  let create = "";
+// function chatting() {
+//   let storeValues = JSON.parse(localStorage.getItem("reply"));
 
-  create = `<div class="chatDiv"><div class="left">Hi!</div></div>`;
-  right = right + create;
+//   let right = "";
+//   let create = "";
 
-  if (storeValues[0] == "hi") {
-    create = `<div class="chatDiv"><div class="right">${storeValues[0]}</div></div>`;
-    document.getElementById("textArea").placeholder = "Your name";
-    right = right + create;
+//   create = `<div class="chatDiv"><div class="left">Hi!</div></div>`;
+//   right = right + create;
 
-    create = `<div class="chatDiv"><div class="left">I am Prasanna venkatesh</div></div>`;
-    right = right + create;
-  }
+//   if (storeValues[0] == "hi") {
+//     create = `<div class="chatDiv"><div class="right">${storeValues[0]}</div></div>`;
+//     document.getElementById("textArea").placeholder = "Your name";
+//     right = right + create;
 
-  if (storeValues.length >= 2) {
-    create = `<div class="chatDiv"><div class="right">My name is ${storeValues[1]}</div></div>`;
-    document.getElementById("textArea").placeholder = "Thank you";
-    right = right + create;
+//     create = `<div class="chatDiv"><div class="left">I am Prasanna venkatesh</div></div>`;
+//     right = right + create;
+//   }
 
-    create = `<div class="chatDiv"><div class="left">Welcome ${storeValues[1]} <a href="./../../pages/home/html/home.html">click here</a> to see my portfolio thank you</div></div>`;
-    right = right + create;
-  }
+//   if (storeValues.length >= 2) {
+//     create = `<div class="chatDiv"><div class="right">My name is ${storeValues[1]}</div></div>`;
+//     document.getElementById("textArea").placeholder = "Thank you";
+//     right = right + create;
 
-  if (storeValues[2] == "thank you") {
-    create = `<div class="chatDiv"><div class="right">${storeValues[2]}</div></div>`;
-    right = right + create;
+//     create = `<div class="chatDiv"><div class="left">Welcome ${storeValues[1]} <a href="./../../pages/home/html/home.html">click here</a> to see my portfolio thank you</div></div>`;
+//     right = right + create;
+//   }
 
-    create = `<div class="chatDiv"><div class="left">&#128522;&#128522;&#128522;</div></div>`;
-    right = right + create;
+//   if (storeValues[2] == "thank you") {
+//     create = `<div class="chatDiv"><div class="right">${storeValues[2]}</div></div>`;
+//     right = right + create;
 
-    setTimeout(function () {
-      window.location.href = "./../../pages/home/html/home.html";
-    }, 1000);
-  }
+//     create = `<div class="chatDiv"><div class="left">&#128522;&#128522;&#128522;</div></div>`;
+//     right = right + create;
 
-  document.getElementById("chatDiv").innerHTML = right;
-}
+//     setTimeout(function () {
+//       window.location.href = "./../../pages/home/html/home.html";
+//     }, 1000);
+//   }
 
-buttonChange();
-chatting();
+//   document.getElementById("chatDiv").innerHTML = right;
+// }
+
+// buttonChange();
+// chatting();
